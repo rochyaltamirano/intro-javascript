4e454a51214bc44c7eddecbc44ebd85e18c8a2b4
Rocio <rochyaltamirano@gmail.com>
2021-02-14 17:02:20 -0300

    instrucción If en JS

---
+++ app3.js
@@ -0,0 +1,12 @@
+/*
+    Sentencia if..else
+*/
+
+var nombre = 'Josefa';
+var esCasado = false;
+
+if (esCasado == true){
+    console.log(nombre + ' es casad@.');
+} else{
+    console.log(nombre + ' es solter@.');
+}
