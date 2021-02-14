c530dd315e95e372d1e88b75a1b76736efcd54a0
Rocio <rochyaltamirano@gmail.com>
2021-02-14 18:03:16 -0300

    Ciclos while y do..while

---
+++ app6.js
@@ -0,0 +1,25 @@
+/*
+    Sentencias While, Do While
+*/
+
+//Ciclo While
+var i = 1;
+while (i <= 10){
+    console.log(i);
+    i++;
+}
+console.log(i);
+
+var i = 10;
+while(i >=1){
+    console.log(i);
+    i--;
+}
+
+//Do..While
+var i = 1;
+do{
+    console.log(i);
+    i++;
+} while (i <= 10)
+console.log(i);
