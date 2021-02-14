064ea8b0706aec7b3b9b43f3dd4480a63ab9db93
Rocio <rochyaltamirano@gmail.com>
2021-02-13 23:19:01 -0300

    variables y tipos de datos en js

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-console.log('Hola a todos');
-console.log(5 + 5);
+var nombre = "Josefa";
+console.log(nombre);
+console.log(typeof(nombre));
+
+var edad = 34;
+console.log(edad);
+console.log(typeof(edad));
+
+edad = 'Cinco';
+console.log(edad);
+console.log(typeof(edad));
+
+var sueldo = 1900.99;
+console.log(sueldo);
+console.log(typeof(sueldo));
+
+var tieneTrabajo = false;
+console.log(tieneTrabajo);
+console.log(typeof(tieneTrabajo));
+
+var puestoDeTrabajo;
+console.log(puestoDeTrabajo);
+
+puestoDeTrabajo = null;
+console.log(puestoDeTrabajo);
