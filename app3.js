03e2c9ef98676e9ae64f8750662c9bc7fa9068cd
Rocio <rochyaltamirano@gmail.com>
2021-02-14 17:17:48 -0300

    if anidados

--- app3.js
+++ app3.js
@@ -10,3 +10,22 @@ if (esCasado == true){
 } else{
     console.log(nombre + ' es solter@.');
 }
+
+//If anidados
+var nombre = 'Luis';
+var edad = 60;
+
+//edad < 12 -> es un niño
+//edad > 11 y edad < 18 -> es un adolescente
+//edad > 17 y edad < 60 -> es un adulto
+//edad > 60 es un anciano
+
+if (edad < 8){
+    console.log(nombre + ' es un niño');
+} else if ((edad > 11) && (edad < 18)){
+    console.log(nombre + ' es un adolescente');
+} else if ((edad >17) && (edad < 60)){
+    console.log(nombre + ' es un adulto');
+} else {
+    console.log(nombre + ' es un anciano');
+}
