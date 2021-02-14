64e1eab483e4e9851bcb2968b653a70b5f24ab3e
Rocio <rochyaltamirano@gmail.com>
2021-02-14 15:53:08 -0300

    ejemplo de uso de operadores matemáticos

--- app.js
+++ app.js
@@ -29,3 +29,31 @@ puestoDeTrabajo = null;
 console.log(puestoDeTrabajo);
 */
 
+
+/*
+Operadores Matemáticos +, -, *. /
+*/
+
+var edadAna, edadMaria, diferenciaEdad;
+var sumaEdades, yearAna, yearMaria, yearActual;
+
+edadAna = 34;
+edadMaria = 28;
+yearActual = 2019;
+
+diferenciaEdad = edadAna - edadMaria;
+sumaEdades = edadMaria + edadMaria;
+
+yearAna = yearActual - edadAna;
+yearMaria = yearActual - edadMaria;
+
+console.log(diferenciaEdad);
+console.log(sumaEdades);
+console.log('Año en que nació Ana ' + yearAna);
+console.log('Año en que nació María ' + yearMaria);
+
+console.log(yearActual * 5);
+console.log(yearActual / 2);
+
+
+
