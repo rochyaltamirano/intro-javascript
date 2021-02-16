7e9b9f2fa069c0612106a689783ff3a67a45f64e
Rocio <rochyaltamirano@gmail.com>
2021-02-15 22:58:18 -0300

    argumentos de una función JS

--- funciones.js
+++ funciones.js
@@ -1,3 +1,6 @@
+/*
+    Ejemplo de usos de funciones
+*/
 function mensaje(msj){
     console.log(msj);
 }
@@ -12,3 +15,31 @@ function mensaje2 (){
 
 var msj = mensaje2();
 console.log(msj);
+
+/*
+    argumentos de una funcion
+*/
+
+function sumar(a, b, c){
+    return a + b + c;
+}
+
+var result = sumar(4, 5, 8);
+console.log('Resultado: ' + result);
+
+//el tercer elemento es opcional para pasar como parámetro
+//por eso se le asigna un valor por defecto
+function sumar2(a, b, c=3){
+    return a + b + c;
+}
+
+var result = sumar2(4, 5);
+console.log('Resultado2: ' + result);
+
+//funciones como expresiones -> funciones anónimas
+var suma = function (a, b, c){
+    return a + b + c;
+}
+
+var res = suma(1, 2, 4);
+console.log(res);
