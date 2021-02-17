017cd998536b9cf700f1a8cb043bc0fe9424b828
Rocio <rochyaltamirano@gmail.com>
2021-02-17 10:54:32 -0300

    seleccionando elementos del DOM

---
+++ calculadora.js
@@ -0,0 +1,14 @@
+//capturo los botones
+const botonNumeros = document.getElementsByName('data-number');
+const botonOpera = document.getElementsByName('data-opera');
+const botonIgual = document.getElementsByName('data-igual')[0];
+const botonDelete = document.getElementsByName('data-delete')[0];
+
+console.log(botonNumeros);
+console.log(botonOpera);
+console.log(botonIgual);
+console.log(botonDelete);
+
+//capturo el resultado
+var result = document.getElementById('result');
+console.log(result);
